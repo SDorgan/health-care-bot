796d2b1681c0de1bde1aeee91495d7063904792d
SDorgan <santidorgan@gmail.com>
2020-07-24 21:38:47 -0300

    resumen para afiliados: agrego tests

--- spec/bot_client_spec.rb
+++ spec/bot_client_spec.rb
@@ -267,4 +267,26 @@ describe 'BotClient' do
     app = BotClient.new(token)
     app.run_once
   end
+
+  xit 'when user non afiliated looks for his resume' do # rubocop:disable RSpec/ExampleLength
+    stub_get_updates(token, '/resumen')
+    body = 'El ID no pertenece a un afiliado'
+    stub_request(:get, "#{ENV['API_URL']}/resumen?id=141733544&from=telegram").to_return(status: 401, body: body, headers: {})
+
+    stub_send_message(token, 'Parece que no estás afiliado a ningún plan, por lo que no podemos mandarte un resumen en este momento.')
+
+    app = BotClient.new(token)
+    app.run_once
+  end
+
+  xit 'when API fails looking up the resume' do # rubocop:disable RSpec/ExampleLength
+    stub_get_updates(token, '/resumen')
+    body = 'Error obteniendo el resumen'
+    stub_request(:get, "#{ENV['API_URL']}/resumen?id=141733544&from=telegram").to_return(status: 500, body: body, headers: {})
+
+    stub_send_message(token, 'Error obteniendo el resumen')
+
+    app = BotClient.new(token)
+    app.run_once
+  end
 end
