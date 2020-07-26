ecf05fe2435c454c39305bfbebbb63ec48a5f263
Patricio Iribarne Catella <patricio_14_94@hotmail.com>
2020-07-26 16:25:30 -0300

    Add test on suspicious sore throat case

--- spec/bot_covid_spec.rb
+++ spec/bot_covid_spec.rb
@@ -255,6 +255,60 @@ describe 'BotClientCovidCommands' do
     app.run_once
   end
 
+  it 'when user test covid diagnosis with sore throat suspicious receives covid suspicious' do # rubocop:disable RSpec/ExampleLength, Metrics/BlockLength
+    options_temp = [
+      [{ "text": '35 o menos', "callback_data": '35' }],
+      [{ "text": '36', "callback_data": '36' }],
+      [{ "text": '37', "callback_data": '37' }],
+      [{ "text": '38 o más', "callback_data": '38' }]
+    ]
+
+    options_yes_no_questions = [
+      [{ "text": 'Sí', "callback_data": 'si' }],
+      [{ "text": 'No', "callback_data": 'no' }]
+    ]
+
+    stub_get_updates_callback_query(token,
+                                    'Cuál es tu temperatura corporal?',
+                                    options_temp,
+                                    '37')
+
+    stub_edit_message_callback_query(token,
+                                     'Percibiste una marcada pérdida de olfato de manera repentina?')
+    stub_get_updates_callback_query(token,
+                                    'Percibiste una marcada pérdida de olfato de manera repentina?',
+                                    options_yes_no_questions,
+                                    'no')
+
+    stub_edit_message_callback_query(token,
+                                     'Percibiste una marcada pérdida del gusto de manera repentina?')
+    stub_get_updates_callback_query(token,
+                                    'Percibiste una marcada pérdida del gusto de manera repentina?',
+                                    options_yes_no_questions,
+                                    'no')
+
+    stub_edit_message_callback_query(token, 'Tenés tos?')
+    stub_get_updates_callback_query(token, 'Tenés tos?',
+                                    options_yes_no_questions,
+                                    'no')
+
+    stub_edit_message_callback_query(token, 'Tenés dolor de garganta?')
+    stub_get_updates_callback_query(token, 'Tenés dolor de garganta?',
+                                    options_yes_no_questions,
+                                    'si')
+
+    body = { "sospechoso": true }
+    stub_request(:post, "#{ENV['API_URL']}/covid")
+      .with(
+        body: { 'id_telegram' => '141733544' }
+      )
+      .to_return(status: 200, body: body.to_json, headers: {})
+    stub_send_message(token, 'Sos un caso sospechoso de COVID. Acércate a un centro médico')
+
+    app = BotClient.new(token)
+    app.run_once
+  end
+
   it 'when user test covid diagnosis with temperature suspicious receives covid suspicious and get error' do # rubocop:disable RSpec/ExampleLength
     options_temp = [
       [{ "text": '35 o menos', "callback_data": '35' }],
