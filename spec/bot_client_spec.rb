7fa3913f3fef84e045e74302c350a4744e893b9b
Patricio Iribarne Catella <patricio_14_94@hotmail.com>
2020-07-26 11:58:44 -0300

    Refactor: move plans spec tests into a separete file

--- spec/bot_client_spec.rb
+++ spec/bot_client_spec.rb
@@ -151,32 +151,4 @@ describe 'BotClient' do
 
     app.run_once
   end
-
-  it 'when no plans, should get a /planes message and respond with no plan' do # rubocop:disable RSpec/ExampleLength
-    stub_get_updates(token, '/planes')
-
-    body = { "planes": [] }
-    stub_request(:get, "#{ENV['API_URL']}/planes")
-      .to_return(status: 200, body: body.to_json, headers: {})
-
-    stub_send_message(token, 'Lo sentimos, parece que no hay planes cargados en el momento.')
-
-    app = BotClient.new(token)
-    app.run_once
-  end
-
-  it 'When plans, should get a /planes message and respond with plans' do # rubocop:disable RSpec/ExampleLength
-    stub_get_updates(token, '/planes')
-    body = { "planes": [
-      { id: 1, 'nombre': 'plan1' },
-      { id: 2, 'nombre': 'plan2' }
-    ] }
-    stub_request(:get, "#{ENV['API_URL']}/planes")
-      .to_return(status: 200, body: body.to_json, headers: {})
-    stub_send_message(token, "Estos son nuestros planes disponibles:\nplan1\nplan2")
-
-    app = BotClient.new(token)
-
-    app.run_once
-  end
 end
