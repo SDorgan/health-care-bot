7a40880041970c6ca557e5a9b3dceefcfc2d9c3a
Patricio Iribarne Catella <patricio_14_94@hotmail.com>
2020-07-26 17:22:07 -0300

    Add test on suspicious last selection question case

--- app/covid/last_selection_rule.rb
+++ app/covid/last_selection_rule.rb
@@ -2,6 +2,6 @@ require_relative './last_selection_question'
 
 class LastSelectionRule
   def process(option)
-    option.eql? CovidLastSelectionQuestion::OPT_NINGUNA['id']
+    !option.eql? CovidLastSelectionQuestion::OPT_NINGUNA[:id]
   end
 end
