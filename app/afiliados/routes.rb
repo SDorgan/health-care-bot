b82dbe4a2988c61fefda3f2390ece9394ecbe2a1
Jia Jun Yang <y_jiajun_16@hotmail.com>
2020-07-28 22:44:23 -0300

    refactor ruta

--- app/afiliados/routes.rb
+++ app/afiliados/routes.rb
@@ -5,21 +5,27 @@ require_relative './afiliados_service'
 module RegistrationRoutes
   include Routing
 
-  on_message_pattern %r{/registracion (?<nombre_plan>.*), (?<nombre>.*), (?<edad>.*)} do |bot, message, args|
-    response = AfiliadosService.post_afiliados(nombre: args['nombre'],
-                                               nombre_plan: args['nombre_plan'],
-                                               edad: args['edad'].to_i,
-                                               cantidad_hijos: 0,
-                                               conyuge: false,
-                                               id_telegram: message.from.id)
-    if response.status == 201
-      bot.api.send_message(chat_id: message.chat.id, text: 'Registración exitosa')
+  on_message_pattern %r{/registracion(?<parametros>.*)} do |bot, message, args|
+    parametros = args['parametros'].split(',')
+    if parametros.length >= 3
+      nombre_plan = parametros[0].squeeze(' ').strip
+      nombre = parametros[1].squeeze(' ').strip
+      edad = parametros[2].to_i
+      cantidad_hijos = 0
+      conyuge = false
+      response = AfiliadosService.post_afiliados(nombre: nombre,
+                                                 nombre_plan: nombre_plan,
+                                                 edad: edad,
+                                                 cantidad_hijos: cantidad_hijos,
+                                                 conyuge: conyuge,
+                                                 id_telegram: message.from.id)
+      if response.status == 201
+        bot.api.send_message(chat_id: message.chat.id, text: 'Registración exitosa')
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: "Registración fallida: #{response.body.force_encoding('UTF-8')}")
+      end
     else
-      bot.api.send_message(chat_id: message.chat.id, text: "Registración fallida: #{response.body.force_encoding('UTF-8')}")
+      bot.api.send_message(chat_id: message.chat.id, text: 'Comando incorrecto, se necesita nombre del plan e información personal. Ej: /registracion NombrePlan, Nombre, {edad}')
     end
-  end
-
-  on_message_pattern %r{/registracion(?<parametros>.*)} do |bot, message, _args|
-    bot.api.send_message(chat_id: message.chat.id, text: 'Comando incorrecto, se necesita nombre del plan e información personal. Ej: /registracion NombrePlan, Nombre, {edad}')
   end
 end
