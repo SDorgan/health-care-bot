fd6484c91b77b0691a96ed394196ac59fd58444c
SDorgan <santidorgan@gmail.com>
2020-08-05 15:07:48 -0300

    Refactor de consistencia de la API: impacto en el bot

--- spec/bot_registration_spec.rb
+++ spec/bot_registration_spec.rb
@@ -42,15 +42,15 @@ describe 'BotClientRegistrationCommands' do
   it 'when user register to plan /registracion without minimum age should get error' do # rubocop:disable RSpec/ExampleLength
     stub_get_updates(token, '/registracion PlanJuventud, Juan, 7')
 
-    body = 'no alcanza el límite mínimo de edad'
+    body = { "mensaje": 'no alcanza el límite mínimo de edad' }
     stub_request(:post, "#{ENV['API_URL']}/afiliados")
       .with(
         body: { 'nombre' => 'Juan', 'nombre_plan' => 'PlanJuventud', 'edad': 7, 'cantidad_hijos': 0, 'conyuge': false, 'id_telegram' => 141_733_544 },
         headers: { 'API_KEY': API_KEY }
       )
-      .to_return(status: 400, body: body, headers: {})
+      .to_return(status: 400, body: body.to_json, headers: {})
 
-    stub_send_message(token, "Registración fallida: #{body}")
+    stub_send_message(token, 'Registración fallida: no alcanza el límite mínimo de edad')
 
     app = BotClient.new(token)
     app.run_once
@@ -59,15 +59,15 @@ describe 'BotClientRegistrationCommands' do
   it 'when user register to plan /registracion with age more than limit should get error' do # rubocop:disable RSpec/ExampleLength
     stub_get_updates(token, '/registracion PlanJuventud, Juan, 80')
 
-    body = 'supera el límite máximo de edad'
+    body = { "mensaje": 'supera el límite máximo de edad' }
     stub_request(:post, "#{ENV['API_URL']}/afiliados")
       .with(
         body: { 'nombre' => 'Juan', 'nombre_plan' => 'PlanJuventud', 'edad': 80, 'cantidad_hijos': 0, 'conyuge': false, 'id_telegram' => 141_733_544 },
         headers: { 'API_KEY': API_KEY }
       )
-      .to_return(status: 400, body: body, headers: {})
+      .to_return(status: 400, body: body.to_json, headers: {})
 
-    stub_send_message(token, "Registración fallida: #{body}")
+    stub_send_message(token, 'Registración fallida: supera el límite máximo de edad')
 
     app = BotClient.new(token)
     app.run_once
@@ -127,15 +127,15 @@ describe 'BotClientRegistrationCommands' do
   it 'when user register to plan /registracion with children more than limit should get error' do # rubocop:disable RSpec/ExampleLength
     stub_get_updates(token, '/registracion PlanJuventud, Juan, 20, hijos-1')
 
-    body = 'este plan no admite hijos'
+    body = { "mensaje": 'este plan no admite hijos' }
     stub_request(:post, "#{ENV['API_URL']}/afiliados")
       .with(
         body: { 'nombre' => 'Juan', 'nombre_plan' => 'PlanJuventud', 'edad': 20, 'cantidad_hijos': 1, 'conyuge': false, 'id_telegram' => 141_733_544 },
         headers: { 'API_KEY': API_KEY }
       )
-      .to_return(status: 400, body: body, headers: {})
+      .to_return(status: 400, body: body.to_json, headers: {})
 
-    stub_send_message(token, "Registración fallida: #{body}")
+    stub_send_message(token, 'Registración fallida: este plan no admite hijos')
 
     app = BotClient.new(token)
     app.run_once
@@ -144,15 +144,15 @@ describe 'BotClientRegistrationCommands' do
   it 'when user register to plan require no spouse /registracion with spouse should get error' do # rubocop:disable RSpec/ExampleLength
     stub_get_updates(token, '/registracion PlanJuventud, Juan, 20, conyuge')
 
-    body = 'este plan no admite conyuge'
+    body = { "mensaje": 'este plan no admite conyuge' }
     stub_request(:post, "#{ENV['API_URL']}/afiliados")
       .with(
         body: { 'nombre' => 'Juan', 'nombre_plan' => 'PlanJuventud', 'edad': 20, 'cantidad_hijos': 0, 'conyuge': true, 'id_telegram' => 141_733_544 },
         headers: { 'API_KEY': API_KEY }
       )
-      .to_return(status: 400, body: body, headers: {})
+      .to_return(status: 400, body: body.to_json, headers: {})
 
-    stub_send_message(token, "Registración fallida: #{body}")
+    stub_send_message(token, 'Registración fallida: este plan no admite conyuge')
 
     app = BotClient.new(token)
     app.run_once
@@ -161,15 +161,15 @@ describe 'BotClientRegistrationCommands' do
   it 'when user register to plan require children /registracion with no children should get error' do # rubocop:disable RSpec/ExampleLength
     stub_get_updates(token, '/registracion PlanFamiliar, Juan, 20')
 
-    body = 'este plan requiere tener hijos'
+    body = { "mensaje": 'este plan requiere tener hijos' }
     stub_request(:post, "#{ENV['API_URL']}/afiliados")
       .with(
         body: { 'nombre' => 'Juan', 'nombre_plan' => 'PlanFamiliar', 'edad': 20, 'cantidad_hijos': 0, 'conyuge': false, 'id_telegram' => 141_733_544 },
         headers: { 'API_KEY': API_KEY }
       )
-      .to_return(status: 400, body: body, headers: {})
+      .to_return(status: 400, body: body.to_json, headers: {})
 
-    stub_send_message(token, "Registración fallida: #{body}")
+    stub_send_message(token, 'Registración fallida: este plan requiere tener hijos')
 
     app = BotClient.new(token)
     app.run_once
@@ -178,15 +178,15 @@ describe 'BotClientRegistrationCommands' do
   it 'when user register to plan require children /registracion with spouse and no children should get error' do # rubocop:disable RSpec/ExampleLength
     stub_get_updates(token, '/registracion PlanFamiliar, Juan, 20, conyuge')
 
-    body = 'este plan requiere tener hijos'
+    body = { "mensaje": 'este plan requiere tener hijos' }
     stub_request(:post, "#{ENV['API_URL']}/afiliados")
       .with(
         body: { 'nombre' => 'Juan', 'nombre_plan' => 'PlanFamiliar', 'edad': 20, 'cantidad_hijos': 0, 'conyuge': true, 'id_telegram' => 141_733_544 },
         headers: { 'API_KEY': API_KEY }
       )
-      .to_return(status: 400, body: body, headers: {})
+      .to_return(status: 400, body: body.to_json, headers: {})
 
-    stub_send_message(token, "Registración fallida: #{body}")
+    stub_send_message(token, 'Registración fallida: este plan requiere tener hijos')
 
     app = BotClient.new(token)
     app.run_once
@@ -195,15 +195,15 @@ describe 'BotClientRegistrationCommands' do
   it 'when user register to plan not allow children /registracion with spouse and children should get error' do # rubocop:disable RSpec/ExampleLength
     stub_get_updates(token, '/registracion PlanPareja, Miriam Perez, 28, hijos-1, conyuge')
 
-    body = 'este plan no admite hijos'
+    body = { "mensaje": 'este plan no admite hijos' }
     stub_request(:post, "#{ENV['API_URL']}/afiliados")
       .with(
         body: { 'nombre' => 'Miriam Perez', 'nombre_plan' => 'PlanPareja', 'edad': 28, 'cantidad_hijos': 1, 'conyuge': true, 'id_telegram' => 141_733_544 },
         headers: { 'API_KEY': API_KEY }
       )
-      .to_return(status: 400, body: body, headers: {})
+      .to_return(status: 400, body: body.to_json, headers: {})
 
-    stub_send_message(token, "Registración fallida: #{body}")
+    stub_send_message(token, 'Registración fallida: este plan no admite hijos')
 
     app = BotClient.new(token)
     app.run_once
@@ -212,15 +212,15 @@ describe 'BotClientRegistrationCommands' do
   it 'when user register to plan not allow children and require spouse /registracion with children should get error' do # rubocop:disable RSpec/ExampleLength
     stub_get_updates(token, '/registracion PlanPareja, Miriam Perez, 28, hijos-1')
 
-    body = 'este plan no admite hijos'
+    body = { "mensaje": 'este plan no admite hijos' }
     stub_request(:post, "#{ENV['API_URL']}/afiliados")
       .with(
         body: { 'nombre' => 'Miriam Perez', 'nombre_plan' => 'PlanPareja', 'edad': 28, 'cantidad_hijos': 1, 'conyuge': false, 'id_telegram' => 141_733_544 },
         headers: { 'API_KEY': API_KEY }
       )
-      .to_return(status: 400, body: body, headers: {})
+      .to_return(status: 400, body: body.to_json, headers: {})
 
-    stub_send_message(token, "Registración fallida: #{body}")
+    stub_send_message(token, 'Registración fallida: este plan no admite hijos')
 
     app = BotClient.new(token)
     app.run_once
